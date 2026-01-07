c8d1808afedc14d63a56773f25dfb959804c52df
Tim Sullivan <timothyj.sullivan1@gmail.com>
2026-01-07 21:17:31 +0000

    adding infrastructure subnet to aca

--- infra/terraform/_aca.tf
+++ infra/terraform/_aca.tf
@@ -11,7 +11,7 @@ resource "azurerm_container_app_environment" "cae" {
   location                   = var.location
   resource_group_name        = azurerm_resource_group.rg.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.laws.id
-  # infrastructure_subnet_id   = azurerm_subnet.aca.id
+  infrastructure_subnet_id   = azurerm_subnet.containerapps.id
 
   tags = local.common_tags
 }
