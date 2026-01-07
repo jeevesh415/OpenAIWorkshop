faee07b318ba9921604369cec9abdfcab0a7c3cf
Tim Sullivan <timothyj.sullivan1@gmail.com>
2026-01-07 18:06:21 +0000

    added NSG link to subnet

--- infra/terraform/_virtual_network.tf
+++ infra/terraform/_virtual_network.tf
@@ -16,4 +16,11 @@ resource "azurerm_subnet" "default" {
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.base.name
     address_prefixes = var.subnet_default_address_space
+
+    private_endpoint_network_policies = "Enabled"
+}
+
+resource "azurerm_subnet_network_security_group_association" "default-nsg-association" {
+  subnet_id                 = azurerm_subnet.default.id
+  network_security_group_id = azurerm_network_security_group.default.id
 }
