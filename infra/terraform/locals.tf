0acaaea69f77c980d836c50fef5e17794519b0ee
Tim Sullivan <timothyj.sullivan1@gmail.com>
2026-01-07 21:39:30 +0000

    moving to lower for naming convention to prevent resource recreation

--- infra/terraform/locals.tf
+++ infra/terraform/locals.tf
@@ -1,11 +1,11 @@
 locals {
   env         = var.environment
-  name_prefix = "${var.project_name}-${local.env}"
+  name_prefix = lower("${var.project_name}-${local.env}")
 
   rg_name               = "rg-${local.name_prefix}-${var.iteration}"
   asp_name              = "asp-${var.project_name}-${local.env}"
   app_name              = "app-${var.project_name}-${local.env}"
-  ai_hub_name           = "aih-${var.project_name}-${local.env}-${var.iteration}"
+  ai_hub_name           = lower("aih-${var.project_name}-${local.env}-${var.iteration}")
   vnet_name             = "vnet-${local.name_prefix}"
   security_group_prefix = "sg-${local.name_prefix}"
   model_endpoint        = "https://${local.ai_hub_name}.openai.azure.com/openai/v1/chat/completions"
